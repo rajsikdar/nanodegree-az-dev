e9e5c3f9521f126b6915c8f99e1195169ff39651
rajsikdar <30749970+rajsikdar@users.noreply.github.com>
2021-12-14 11:41:25 -0500

    Updating directory

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cmsblobstoragedb'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '2yhkfyGdV50USa+onvup7ndXPPcukZFmjYJMSHgLcOpFnZ/+O0jnRqjbkPAqVn9M1D5x+or7VQwe4llRiOfsjw=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
+
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'rajivcmsdb.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cmsdb'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'cms_user'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Ilovekoko22'
+    # Below URI may need some adjustments for driver version, based on your OS, if running locally
+    # For Mac and Linux
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
+
+    # For Windows
+    # SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=SQL+Server'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    ### Info for MS Authentication ###
+    ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
+    CLIENT_SECRET = "4HS7Q~AAeztv~_ml46dg1hUyZ.hA4eilyvj6J"
+    # In your production app, Microsoft recommends you to use other ways to store your secret,
+    # such as KeyVault, or environment variable as described in Flask's documentation here:
+    # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
+    # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    # if not CLIENT_SECRET:
+    #     raise ValueError("Need to define CLIENT_SECRET environment variable")
+
+    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+
+    CLIENT_ID = "e9bc486c-bf05-4b6c-aecf-69e982dba29a"
+
+    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+
+    # You can find the proper permission names from this document
+    # https://docs.microsoft.com/en-us/graph/permissions-reference
+    SCOPE = ["User.Read"] # Only need to read user profile for this app
+
+    SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
