3fbbf3dbe4d0b8eb211e3c8ecf9f945a18b198a7
rajsikdar <30749970+rajsikdar@users.noreply.github.com>
2021-12-14 10:05:12 -0500

    Update __init__.py

--- rajiv-ms-azure-cms-project-starter/FlaskWebProject/__init__.py
+++ rajiv-ms-azure-cms-project-starter/FlaskWebProject/__init__.py
@@ -10,10 +10,14 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+# Add any logging levels and handlers with app.logger
+wsgi_app = app.wsgi_app
+app.logger.setLevel(logging.WARNING)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
 Session(app)
 db = SQLAlchemy(app)
-wsgi_app = app.wsgi_app
 login = LoginManager(app)
 login.login_view = 'login'
 
