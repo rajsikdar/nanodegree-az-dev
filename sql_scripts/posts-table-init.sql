05ca8899aff97392ec11b2a39518e0ee612e427c
Rajiv Sikdar <30749970+rajsikdar@users.noreply.github.com>
2026-01-17 10:10:41 -0500

    Update posts-table-init.sql

--- sql_scripts/posts-table-init.sql
+++ sql_scripts/posts-table-init.sql
@@ -12,7 +12,7 @@ CREATE TABLE POSTS(
 
 INSERT INTO dbo.posts (title, author, body, user_id)
 VALUES (
-    'Lorem ipsum dolor sit amet',
+    'Lorem ipsum dolor sit amett',
     'John Green Smith',
     'Proin sit amet mi ornare, ultrices augue quis, facilisis tellus. Quisque neque dui, tincidunt sed volutpat quis, maximus sed est. Sed justo orci, rhoncus ac nulla eu, rhoncus luctus justo. Etiam maximus, felis eu varius fermentum, libero orci egestas purus, id condimentum mauris orci nec nibh. Vivamus risus ipsum, semper vel nibh in, suscipit commodo massa. Suspendisse non velit vitae neque condimentum viverra vel eget enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus fermentum sagittis ligula et fringilla. Aenean nec lacinia lacus.',
     1
