dacdb9d8e2a092058e0162f9506441edbb6d1eef
Rajiv Sikdar <30749970+rajsikdar@users.noreply.github.com>
2026-01-20 14:06:18 -0500

    Update forms.py

--- FlaskWebProject/forms.py
+++ FlaskWebProject/forms.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
+    remember_me = BooleanField('Remember Me (User)')
     submit = SubmitField('Sign In')
 
 class PostForm(FlaskForm):
